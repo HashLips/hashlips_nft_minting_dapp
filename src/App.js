aa583d44453fc012417d3fabce005154cf11bb0e
edenkozlov <96761592+edenkozlov@users.noreply.github.com>
2022-11-20 04:59:24 -0500

    .mint

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-    .PublicSaleMint(mintAmount)
+    .Mint(mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
